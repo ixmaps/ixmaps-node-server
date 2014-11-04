6162c8e6ffd84ed8f29f2b93e674377304457f23
Vid <vid_github@zooid.org>
2014-11-04 09:57:26 -0500

    handle submitted domains and trsets

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 /* jslint node: true */
 'use strict';
 
-var http = require('http');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8181;
@@ -16,9 +15,9 @@ var markdown = require( "markdown" ).markdown;
 
 var dePost = bodyParser.json();
 var traceroute = require('./lib/traceroute');
+var requests = require('./lib/requests');
 // when to refresh checked hosts (10 minutes)
 var RECHECK_SECONDS = 600 * 1000;
-var TRSETS_BASE = 'http://ixmaps.ca/trsets/';
 
 // state variables
 var state = {
@@ -27,6 +26,7 @@ var state = {
 // hosts that have been traced
   processedHosts : {},
   // hosts queued to be traced
+  // format: { domain: <domain>, command: <command>, args: <args> }
   queuedHosts : [],
   // are we currently processing a queued host?
   processingHost : false
@@ -61,37 +61,16 @@ router.get('/api/state', function(req, res) {
 // queue a host
 router.post('/api/requests', dePost, function(req, res) {
   var incoming = req.body;
-  // URL submitted on Chrome access
-  // a URL or TRSET refererence
-  incoming.requests.forEach(function(request) {
-    if (request.type === 'chrome.completed') {
-      state.queuedHosts = state.queuedHosts.concat(incoming.requests);
-    } else if (request.type === 'submitted') {
-      var submitted = [];
-        var data = request.data;
-        if (!data) {
-          console.log('invalid submission', data);
-          return;
-        }
-        // it's a URL
-        if (data.match(/https?:\/\//i)) {
-
-        // see if it's a trset
-        } else {
-          retrieve(TRSETS_BASE + data, function(err, res) {
-            if (err) {
-              console.log('submitted trset failed', err);
-            } else {
-              console.log(res);
-            }
-          });
-        }
+  // process individual requests
+  requests.processRequests(incoming, function(err, request) {
+    if (err) {
+      console.log('requests err', err);
     } else {
-      console.log('unknown request', request);
+      state.queuedHosts = state.queuedHosts.concat(request);
+      console.log('added', request);
     }
   });
 
-  console.log('added', incoming.type, incoming.requests.length);
   res.end();
 });
 
@@ -109,10 +88,10 @@ function processQueue() {
   }
   var nextHost = state.queuedHosts.shift();
   if (nextHost) {
-    var destination = nextHost.properties.domain;
+    var destination = nextHost.domain;
     // there's a destination and it hasn't processed or hasn't been processed recently
     var doProcess = !state.processedHosts[destination] || state.processedHosts[destination] < (new Date().getTime() - RECHECK_SECONDS);
-    console.log('processing', nextHost.command, nextHost.properties.domain, 'doProcess', doProcess, 'remaining', state.queuedHosts.length);
+    console.log('processing', nextHost.command, nextHost.domain, 'doProcess', doProcess, 'remaining', state.queuedHosts.length);
     if (doProcess) {
       // used to link hops
       var hops = [], traceID = destination + '@' + new Date().getTime();
@@ -138,24 +117,10 @@ function processQueue() {
 
       state.processingHost = new Date().getTime();
       state.processedHosts[destination] = new Date().getTime();
-    	traceroute.stream(destination, processHops, doneTrace, { command : nextHost.command, parameters: nextHost.parameters});
+    	traceroute.stream(destination, processHops, doneTrace, { command : nextHost.command, args: nextHost.args});
       return;
     }
   }
   // nothing to process, wait around
   setTimeout(processQueue, 100);
-}
-
-function retrieve(uri, callback) {
-  var buffer;
-  http.get(uri, function(res) {
-  res.on('data', function (chunk) {
-    buffer += chunk;
-  });
-  res.on('end', function() {
-    callback(null, buffer);
-    });
-  }).on('error', function(e) {
-    callback(e);
-  });
 }
