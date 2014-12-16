1b541f65ed2ff2ae82a1d32bdc44ba95b353cd3f
Vid <vid_github@zooid.org>
2014-12-16 12:59:29 -0500

    multiple passes with unique routes

--- rt.js
+++ rt.js
@@ -1,6 +1,20 @@
-var rawtrace = require('./lib/socket-trace.js');
+var rawtrace = require('./lib/socket-trace.js'), _ = require('lodash');
 
+var passes = {}, lastPass = -1, lastSource = null;
 // dest, timeout, interval
 rawtrace({ dest: process.argv[2], timeout: 9000, interval: 10, count: 4, maxTTL: 32}, function(err, res) {
-  console.log(err, res);
+  if (err) {
+    console.log('err', err);
+  } else {
+    console.log('res', res);
+    if (lastPass != res.pass) {
+      lastPass = res.pass;
+      passes[res.pass] = 'graph TD;';
+    } else {
+      passes[res.pass] += '\n  ' + lastSource + '-->' + res.source;
+    }
+    lastSource = res.source;
+  }
+}, function() {
+  console.log(_.uniq(Object.keys(passes).map(function(k) { return passes[k]; })));
 });
