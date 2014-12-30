0dc201e8e5614907402e8ffb3d57abe1066760e9
Vid <vid_github@zooid.org>
2014-12-30 14:25:32 -0500

    making code clearer

--- test/traceroute.js
+++ test/traceroute.js
@@ -13,11 +13,11 @@ describe('parseNix', function () {
 
   describe('parse()', function () {
     it('should parse a route', function () {
-      var t1 = ['traceroute to 10.5.128.98, 30 hops max, 60 byte packets',
+      var buffer = ['traceroute to 10.5.128.98, 30 hops max, 60 byte packets',
         ' 1 192.168.0.1 0.994 ms 0.444 ms 0.449 ms 0.426 ms',
         ' 2 * * * *',
         ' 3 10.6.0.157 40.233 ms 106.772 ms 62.710 ms *'].join('\n');
-      var res = parse.parse(t1);
+      var res = parse.parse(buffer);
 
       expect(res.length).to.be(3);
 
@@ -28,13 +28,17 @@ describe('parseNix', function () {
           expect(h1).to.be.equal('192.168.0.1');
         });
         it('should have timeouts for the second hop', function () {
-          expect(h1).to.eql(h2);
-          expect(res[1][h2].every(function(i) { return i === -1; })).to.be(true);
+          expect(h1).to.equal(h2);
+          expect(res[1][h2].every(function(i) {
+            return i === -1;
+          })).to.be(true);
         });
       });
       describe('validate hops', function () {
         it('should have one hop result for each line', function () {
-          expect(res.every(function(h) { return Object.keys(h).length === 1; })).to.be(true);
+          expect(res.every(function(h) {
+            return Object.keys(h).length === 1;
+          })).to.be(true);
         });
 
         it('should have an IP address for each hop', function () {
