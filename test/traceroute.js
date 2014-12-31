df0732e38b7e27dcebd3ef1cc001b2d0a8377bdc
Vid <vid_github@zooid.org>
2014-12-31 18:43:12 -0500

    updating to support better definition (wip)

--- test/traceroute.js
+++ test/traceroute.js
@@ -6,9 +6,11 @@
 
 'use strict';
 
-var expect = require('expect.js'), net = require('net');
+var expect = require('expect.js'), net = require('net'), _ = require('lodash');
 
 var parse = require('../lib/parseNix');
+
+var NAT = '192.168.0.1';
 
 describe('parseNix', function () {
 
@@ -18,46 +20,53 @@ describe('parseNix', function () {
         'traceroute to 10.5.128.98, 30 hops max, 60 byte packets',
         ' 1 192.168.0.1 0.994 ms 0.444 ms 0.449 ms 0.426 ms',
         ' 2 * * * *',
-        ' 3 10.6.0.157 40.233 ms 106.772 ms 62.710 ms *' /*,
-        ' 4 206.80.192.221 127.569 ms 216.161.182.121  185.214 ms *',
-        ' 5 208.225.64.50  35.931 ms !H *  39.970 ms !H' */
+        ' 3 10.6.0.157 40.233 ms 106.772 ms 62.710 ms *',
+        ' 4 208.225.64.50  35.931 ms !H *  39.970 ms !H',
+        ' 5 206.80.192.221 127.569 ms 216.161.182.121  185.214 ms *'
         ].join('\n');
-      var res = parse.parse(buffer);
+      var res = parse.parse(buffer, 4);
 
-      expect(res.length).to.be(3);
+      expect(res.length).to.be(5);
 
-      var h1 = Object.keys(res[0])[0];
-      var h2 = Object.keys(res[1])[0];
       describe('validate addresses', function () {
         it('should be a NAT address', function () {
-          expect(h1).to.be.equal('192.168.0.1');
+          expect(res[0].every(function(s) {
+            return s.ip === NAT;
+          })).to.be(true);
         });
         it('should have timeouts for the second hop', function () {
-          expect(h1).to.equal(h2);
-          expect(res[1][h2].every(function(i) {
-            return i === -1;
+          expect(res[1].every(function(s) {
+            return s.rtt === -1 && s.ip === NAT;
           })).to.be(true);
         });
       });
       describe('validate hops', function () {
-        it('should have one hop result for each line', function () {
-          expect(res.every(function(h) {
-            return Object.keys(h).length === 1;
+        it('should have four results for regular hops', function () {
+          expect(_.first(res, 3).every(function(h) {
+            return h.length === 4;
           })).to.be(true);
         });
 
         it('should have an IP address for each hop', function () {
           expect(res.every(function(h) {
-            var ip = Object.keys(h)[0];
-            return net.isIP(ip);
+            return h.every(function(s) {
+              return net.isIP(s.ip);
+            });
           })).to.be(true);
         });
 
-        it('should have four results for each hop', function () {
-          expect(res.every(function(h) {
-            var hops = h[Object.keys(h)[0]];
-            return hops.length === 4;
-          })).to.be(true);
+      });
+      describe('handle error conditions', function() {
+        // missing values will be padded with -1
+        it('should ignore error conditions', function() {
+          expect(res[3].length).to.be(3);
+        });
+      });
+      describe('handle router change', function() {
+        it('should switch to the new address', function() {
+          expect(res[4].length).to.be(3);
+          expect(res[4][0].ip).to.be('206.80.192.221');
+          expect(res[4][1].ip).to.be('216.161.182.121');
         });
       });
     });
